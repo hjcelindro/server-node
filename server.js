8b9ee0d792c6d8e54837cd834a0db6205982385d
hjcelindro <hjcelindro@yahoo.co.uk>
2016-01-27 21:26:57 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mqtt = require('mqtt');
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+    console.log('listening to port 3000');
+});
+
+var client = mqtt.connect('mqtt://54.200.3.119:1883');
+
+io.sockets.on('connection',function(socket){
+    socket.on('subscribe',function(data){
+        console.log('Subscribing to :'+data.topic);
+        client.subscribe(data.topic);
+    });
+});
+
+io.on('connection', function(socket){
+     console.log('a user connected');
+    socket.on('disconnect', function(){
+            console.log('user disconnected');
+    });
+});
+
+client.addListener('mqttData',function(topic,payload){
+    console.log(topic +" : "+payload);
+    io.sockets.emit('mqtt',{'topic':String(topic), 'payload':String(payload)});
+});
