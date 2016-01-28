62b1f7d7d189aa5338c5dd595c8e1e3a91e74aa3
hjcelindro <hjcelindro@yahoo.co.uk>
2016-01-28 13:50:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ io.sockets.on('connection',function(socket){
     });
 });
     
-io.on('connection', function(socket){
+io.sockets.on('connection', function(socket){
      console.log('a user connected');
     socket.on('disconnect', function(){
             console.log('user disconnected');
