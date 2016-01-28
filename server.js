923610d05937fd79e0df9b2d30955cc0d20f50b4
hjcelindro <hjcelindro@yahoo.co.uk>
2016-01-28 20:13:49 +0000

    Update server.js
    express app.get

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mqtt = require('mqtt');
 
+/*
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
-});
+});*/
 
 http.listen(3000, function(){
     console.log('listening to port 3000');
@@ -17,10 +18,6 @@ io.sockets.on('connection',function(socket){
     socket.on('subscribe',function(data){
         console.log('Subscribing to :'+data.topic);
         client.subscribe(data.topic);
-    });
-    socket.on('mqtt',function(data){
-        console.log("js "+data.topic+" "+data.payload);
-        io.sockets.emit('mqtt',{'topic':String(data.topic), 'payload':String(data.payload)});
     });
 });
     
@@ -36,5 +33,8 @@ io.sockets.on('connection', function(socket){
 
 client.on('message',function(topic,message){
     //console.log("Client.on"+String(message)+ " "+String(topic));
+    app.get('/', function(req, res){
+        res.sendFile(__dirname + '/index.html');
+    });
     io.sockets.emit('mqtt',{'topic':String(topic), 'payload':String(message)});
 });
