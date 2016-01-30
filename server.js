7c2a322bab439080ea2a0924ed7abc0992f61884
hjcelindro <hjcelindro@yahoo.co.uk>
2016-01-30 21:05:12 +0000

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ io.sockets.on('connection', function(socket){
 
 client.on('message',function(topic,message){
     console.log("Client.on"+String(message)+ " "+String(topic));
-    io.sockets.emit('mqtt',{'topic':String(topic), 'payload':String(message)});
+    //io.sockets.emit('mqtt',{'topic':String(topic), 'payload':String(message)});
+    io.sockets.socket(clients[0]).emit('mqtt',{'topic':String(topic), 'payload':String(message)});
     console.log(manufacturer);
 });
