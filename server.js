c5a60810c3377828e1522e3918b184c879f0356a
hjcelindro <hjcelindro@yahoo.co.uk>
2016-01-30 19:57:40 +0000

    Update server.js

--- server.js
+++ server.js
@@ -24,9 +24,6 @@ io.sockets.on('connection',function(socket){
     socket.on('mqtt',function(data){
         console.log("mqttjs: "+data.payload);
         io.sockets.emit('mqtt',{'topic':String(data.topic), 'payload':String(data.payload)});
-        var thetopic = data.topic;
-        manufacturer = "/"+thetopic.split("/",2)
-        console.log(manufacturer);
     });
 });
     
