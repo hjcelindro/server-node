5713ba9e34c48cd22292e14bce576057cbb930d0
hjcelindro <hjcelindro@yahoo.co.uk>
2016-01-30 21:49:08 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var mqtt = require('mqtt');
 
 var manufacturer;
-var clients={};
+var clients=[];
 
 
 app.get('/:manufacturer', function(req, res){
@@ -37,20 +37,8 @@ io.sockets.on('connection', function(socket){
     socket.on('register',function(name){
         clients[socket.id] = name.name;
         console.log(clients[socket.id]);
+        clients.push(socket.id);
         console.log('clients:'+clients);
-        
-        client.on('message',function(topic,message){
-    console.log("Client.on"+String(message)+ " "+String(topic));
-    var split = topic.split('/');
-    manufacturer = split[1];
-    //io.sockets.emit('mqtt',{'topic':String(topic), 'payload':String(message)});
-    if(manufacturer==clients[socket.id]){
-        io.sockets.connected[clients[socket.id]].emit('mqtt_sub',{'topic':String(topic), 'payload':String(message)});
-    }
-    //io.sockets.socket(clients[0]).emit('mqtt',{'topic':String(topic), 'payload':String(message)});
-    console.log(manufacturer);
-});
-        
     });
      console.log('a user connected'+socket.id);
     socket.on('disconnect', function(){
