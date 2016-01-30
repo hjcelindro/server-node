468b1e47c8904636c67bcb1e8e7ff9bf3691abc7
hjcelindro <hjcelindro@yahoo.co.uk>
2016-01-30 22:00:39 +0000

    Update server.js

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ io.sockets.on('connection', function(socket){
         clients[socket.id] = name.name;
         console.log(clients[socket.id]);
         clients.push(socket.id);
-        for(i=0;i<clients.length;i++){
-            var client=clients[i];
-            console.log('clients:'+client[socket.id]);
-        }
+        //for(i=0;i<clients.length;i++){
+          //  var client=clients[i];
+        //    console.log('clients:'+client[socket.id]);
+        //}
     });
      console.log('a user connected'+socket.id);
     socket.on('disconnect', function(){
@@ -53,10 +53,10 @@ client.on('message',function(topic,message){
     console.log("Client.on"+String(message)+ " "+String(topic));
     var split = topic.split('/');
     manufacturer = split[1];
-    //io.sockets.emit('mqtt',{'topic':String(topic), 'payload':String(message)});
-    if(manufacturer==clients[socket.id]){
-        io.sockets.connected[clients[socket.id]].emit('mqtt_sub',{'topic':String(topic), 'payload':String(message)});
-    }
+    io.sockets.emit('mqtt',{'topic':String(topic), 'payload':String(message)});
+    //if(manufacturer==clients[socket.id]){
+    //    io.sockets.connected[clients[socket.id]].emit('mqtt_sub',{'topic':String(topic), 'payload':String(message)});
+    //}
     //io.sockets.socket(clients[0]).emit('mqtt',{'topic':String(topic), 'payload':String(message)});
-    console.log(manufacturer);
+    //console.log(manufacturer);
 });
