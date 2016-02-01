812d58de68d77415822a0f29348360b073c4572d
hjcelindro <hjcelindro@yahoo.co.uk>
2016-02-01 19:32:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ io.sockets.on('connection',function(socket){
     socket.on('mqtt',function(data){
         var manufacturerID = checkID(manufacturer);
         console.log("manufacturerID: "+checkID(manufacturer));
-        io.sockets.to(checkID(manufacturer)).emit('mqtt',{'topic':String(topic), 'payload':String(message)});
+        io.to(checkID(manufacturer)).emit('mqtt',{'topic':String(topic), 'payload':String(message)});
     });
     
     socket.on('register',function(name){
@@ -56,7 +56,7 @@ io.sockets.on('connection',function(socket){
     //var manufacturerID = checkID(manufacturer);
     //console.log("manufacturerID: "+checkID(manufacturer));
     //io.sockets.to(checkID(manufacturer)).emit('mqtt',{'topic':String(topic), 'payload':String(message)});
-    io.sockets.to(checkID(manufacturer)).emit('mqtt',{'topic':String(topic), 'payload':String(message)});
+    io.to(checkID(manufacturer)).emit('mqtt',{'topic':String(topic), 'payload':String(message)});
 });
     
 });
