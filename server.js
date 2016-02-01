c9f244a8c5e0137b68f6fa49fc5dad8f353f89a1
hjcelindro <hjcelindro@yahoo.co.uk>
2016-02-01 20:14:22 +0000

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ function checkID(client){
             value = key;
     }*/
     for(var i=0;i<clients.length;i++){
-        if(client==(clients[i]){
+        if(client==(clients[i])){
            value = client[i];
            console.log(clients[i]);
         }
