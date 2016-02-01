048cf1fbf2f2d3c93d6006514e5ef7a192ef3273
hjcelindro <hjcelindro@yahoo.co.uk>
2016-02-01 20:13:31 +0000

    Update server.js

--- server.js
+++ server.js
@@ -62,10 +62,16 @@ client.on('message',function(topic,message){
 function checkID(client){
     var value = "";
     console.log('client: '+client);
-    for(var key in clients){
+    /*for(var key in clients){
         console.log('key: '+key);
         if (clients[key] == client)
             value = key;
+    }*/
+    for(var i=0;i<clients.length;i++){
+        if(client==(clients[i]){
+           value = client[i];
+           console.log(clients[i]);
+        }
     }
     console.log("Key: "+value);
     return value;
