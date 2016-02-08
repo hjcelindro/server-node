72099f8683fec65c874dd4aa970e214ff46cd7dc
hjcelindro <hjcelindro@yahoo.co.uk>
2016-02-08 21:51:05 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,109 +1,85 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mqtt = require('mqtt');
-
-var manufacturer;
-var clients=[];
-var items = [];
-var data;
-
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'br-cdbr-azure-south-a.cloudapp.net', //using microsoft azure mySQL
-    user: 'bbbfe6f5303166',
-    password: '6a070d81',
-    database: 'RFIDtags'
-});
-
-//-----------------DATABASE CONNECTIONS-------
-
-//connect to the database
-connection.connect(function(err){
-    if(err) { //if connection fails
-        console.error("error connecting: "+err.stack); //display error code and message
-        return;
-    }
-    console.log("Connected as: "+connection.threadId); //confirmation of connection with threadID
-});
-
-//----------------------------------
-
-
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
-http.listen(3000, function(){
-    console.log('listening to port 3000');
-});
-
-var client = mqtt.connect('mqtt://54.200.3.119:1883');
-
-io.sockets.on('connection',function(socket){
-    
-    var addedClient = false;
-    
-    socket.on('subscribe',function(data){
-        console.log('Subscribing to :'+data.topic);
-        client.subscribe(data.topic);
-        var topic =data.topic;
-        var split = topic.split('/');
-        manufacturer = split[1];
-    });
-    
-    socket.on('mqtt',function(data){
-    });
-    
-    socket.on('register',function(name){
-
-        socket.emit('update_clients',name);
-        socket.join(name); //join room for the manufacturer
-            
-    });
-     console.log('a user connected'+socket.id);
-    socket.on('disconnect', function(){
-            console.log('user disconnected');
-    });
-    
-});
- 
-client.on('message',function(topic,message){
-    console.log("Client.on"+String(message)+ " "+String(topic));
-    var split = topic.split('/');
-    manufacturer = split[1];
-    //io.to(manufacturer).emit('mqtt',{'topic':String(topic), 'payload':String(message)});
-    io.to(manufacturer).emit('mqtt',{'topic':String(topic), 'payload':String(data)});
-    console.log(items);
-    searchDatabase();
-});
-
-
-//search Database
-
-function searchDatabase(){
-    console.log("Manufacturer: "+manufacturer);
-
-    //-----this is a query function that gets rfid data from the online database and compares with reader values                
-    connection.query('SELECT * FROM rfid',function(err,rows){
-        if(err)throw err;
-        else{
-            console.log('Data receieved from database'); //display message that data has been acquired from the database
-            for(var i=0; i<rows.length;i++){
-                var DBmanufacturer = rows[i].item_manufacturer;
-                var tagid = rows[i].item_rfid; //to make coding easier
-                var loc = rows[i].item_location;
-                if(DBmanufacturer===manufacturer){
-                    console.log('items for manufacturer: '+DBmanufacturer);
-                    data = {id:tagid,location:loc};
-                    items.push(data);
-                    //items.push({id:tagid,location:loc});
-                }
+    <head>
+        <h1>Real Time</h1>
+        <title>Testing...</title>
+        <style>
+            table, th, td {
+                border: 1px solid black;
+                border-collapse: :collapse;
             }
-            //for (var i=0;i<items.length;i++){
-              //  console.log(items);
-            //}
-        }//END ELSE STATEMENT
-    }); //END QUERY
-} //END searchDatabase();
+            th, td {
+                padding: 10px;
+            }
+        </style>
+        <script src="/socket.io/socket.io.js"></script>
+        <script src="http://code.jquery.com/jquery-1.11.1.js"></script>
+
+        <script type="text/javascript">
+
+            var socket = io.connect('http://54.200.3.119:3000');
+
+            socket.on('connection',function(){
+
+                socket.on('subscribe',function(msg){
+                });
+            });
+            socket.on('mqtt',function(msg){
+                addMessage(msg);
+            });
+            socket.on('mqtt',function(msg){
+                $('table').append('<tr><td>'+JSON.stringify(msg.id)+'</td><br><td>'+String(msg.location)+'</td></tr>');
+            });
+            socket.on('update_clients',function(name){
+                //updateClients(name);
+                window.alert("Welcome "+name+'!');
+            });
+            
+            function addMessage(message){
+                var text = document.createTextNode(message.payload),
+                    el = document.createElement('li'),
+                    messages = document.getElementById('messages');
+
+                el.appendChild(text);
+                messages.appendChild(el);
+            }
+            
+            function subscribeToTopic(){
+                var textbox = document.getElementById("textbox");
+                var output = document.getElementById("output");
+                var name = textbox.value;
+                var manufacturer = name.substr(0,1).toUpperCase() + name.substr(1);
+                output.value = "You are Subscribed, "+manufacturer;
+                
+                var subscribe = 'manufacturer/'+manufacturer;
+                socket.emit('subscribe',{topic: subscribe});
+                socket.emit('register',manufacturer);
+                
+                $('div').show('clients');
+                $('div').show('table');
+                
+            }
+    
+        </script>
+    </head>
+    <body>
+        <form action=" ">
+            <fieldset>
+                <label>Type your Company name: </label>
+                <input name="topic" id="textbox" type= "text" align="right" size="30"/></br>
+                <input type="button" value="Subscribe" onclick = "subscribeToTopic()"/>
+                <input type="text" id="output"  size = "50"/>
+            </fieldset>
+        </form>
+        <div>
+            <div id = 'clients' style="display:none"><h2>Welcome Client!</h2></div>
+            <ul id = 'messages'><h3>Messages</h3></ul>
+        </div>
+        <div id='table'>
+            <table id = "t1" style='width:60%' >
+                <tr>
+                    <th>Tag ID</th>
+                    <th>Location</th>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
