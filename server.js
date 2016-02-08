e2ec58ed4855aae0ce8f909b4d01fc6adbc24dd3
hjcelindro <hjcelindro@yahoo.co.uk>
2016-02-08 20:47:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,21 @@ function searchDatabase(){
     console.log("Manufacturer: "+manufacturer);
 
     //-----this is a query function that gets rfid data from the online database and compares with reader values
-    connection.query('SELECT * FROM rfid',function(err,rows){
+    connection.query('select item_rfid, item_manufacturer from ' +rfid+' where item_manufacturer = '+manufacturer+"'",
+    function(err,result,fields){
+        if(err) throw err;
+        else {
+            console.log('Items with manufacturer '+manufacturer);
+            console.log('----------------------------------------');
+            for(var i in result){
+                var found = result[i];
+                console.log(found.item_rfid);
+            }
+        }
+    });
+        
+        
+    /*connection.query('SELECT * FROM rfid',function(err,rows){
         if(err)throw err;
         else{
             console.log('Data receieved from database'); //display message that data has been acquired from the database
@@ -95,29 +109,6 @@ function searchDatabase(){
                     console.log(tagid);
                 }
             }
-               /* for(var i=0;i<rows.length;i++){
-
-                    var tagid = rows[i].item_rfid; //to make coding easier
-                    manufacturer = rows[i].item_manufacturer;
-                    var location = rows[i].item_location;
-
-                    if(tagid===string_id){ //compares with the RFID scanned
-                        console.log("Manufacturer of item "+tagid+" is "+manufacturer+" in Location: "+location); //output display on app side terminal
-                        console.log("Notify "+manufacturer+" of new item");// output display on terminal
-                        console.log("Item to notify: "+scanned_rfid+" to manufacturer: "+manufacturer);
-
-                        message = "Manufacturer: "+manufacturer+"\nAn item with RFID "+tagid+
-                            " has been sent to the centre"+"\n Location: "+location;
-
-                        console.log("PUBLISHED:"+"Manufacturer: "+manufacturer+"\nAn item with RFID "+tagid+" has been sent to the centre");
-                        io.on('connection', function(socket){
-                            socket.on('chat message', function(topic,payload,packet){
-                                io.emit('mqtt message', message);
-                                console.log('mqtt message: ' + message);
-                            });
-                        });
-                    }//END IF
-                } *///END FOR LOOP
         }//END ELSE STATEMENT
-    }); //END QUERY
+    }); //END QUERY*/
 } //END searchDatabase();
