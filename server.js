54fc68012bf976e9a3f4507073707072b20ed35e
hjcelindro <hjcelindro@yahoo.co.uk>
2016-03-06 18:47:31 +0000

    Update server.js

--- server.js
+++ server.js
@@ -145,9 +145,10 @@ function searchManufacturerDatabase(){
                 var DBmanufacturer = rows[i].item_manufacturer;
                 var tagid = rows[i].item_rfid; //to make coding easier
                 loc = rows[i].item_location;
-                searchSensorDatabase(loc);
+                //searchSensorDatabase(loc);
                 io.to('All').emit('mqtt',{'topic':'manufacturer/All', 'payload':{id:tagid,location:loc,manufacturer:DBmanufacturer,message:message}});
                 if(DBmanufacturer===manufacturer){
+                    searchSensorDatabase(loc);
                     data = {id:tagid,location:loc,manufacturer:DBmanufacturer,message:message};
                     items.push(data);
                     io.to(manufacturer).emit('mqtt',{'topic':String(topic), 'payload':data});   
