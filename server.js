61de8c1c3cc47dcb7a2d9c6ba4c06a01c3fd2edd
hjcelindro <hjcelindro@yahoo.co.uk>
2016-03-08 17:54:56 +0000

    Update server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ function searchManufacturerDatabase(){
     console.log("Left Join Table");
     var pre_query = new Date().getTime();
     //-----this is a query function that gets rfid data from the online database and compares with reader values                
-    connection.query('SELECT rfid.item_rfid,rfid.item_manufacturer,rfid.item_location, Sensor.location, Sensor.Temperature FROM rfid INNER JOIN Sensor ON rfid.item_location=Sensor.location',function(err,rows){
+    connection.query('SELECT rfid.item_rfid,rfid.item_manufacturer,rfid.item_location, Sensor.location, Sensor.idSensor, readings.idSensor, readings.time, readings.dataReading FROM rfid INNER JOIN Sensor ON rfid.item_location=Sensor.location inner join readings on Sensor.location=readings.idSensor',function(err,rows){
         if(err)throw err;
         else{
             var post_query = new Date().getTime();
