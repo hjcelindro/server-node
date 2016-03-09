6462ffbb17b7c97bbe90b1d130d9d0f47bed1618
hjcelindro <hjcelindro@yahoo.co.uk>
2016-03-09 11:41:44 +0000

    Update server.js

--- server.js
+++ server.js
@@ -111,11 +111,16 @@ io.sockets.on('connection',function(socket){
         //console.log("Time from mqtt to client: "+mqtt_time);
     });
     
+    //socket.on('client response',function(data){
+    //    client_res=data;
+    //    client.publish('response/manufacturer',client_res);
+    //});
+});
+
+io.sockets.on('response',function(socket){
     socket.on('client response',function(data){
         client_res=data;
         client.publish('response/manufacturer',client_res);
-        //io.emit('data_change',{'topic':String(topic), 'payload':data});
-        //searchManufacturerDatabase();
     });
 });
  
