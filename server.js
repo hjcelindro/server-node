e97850750ccfbc977bf88bf2bad441518c3fbd03
hjcelindro <hjcelindro@yahoo.co.uk>
2016-03-09 15:20:35 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,110 +1,177 @@
-    <head>
-        <h1>Real Time</h1>
-        <title>Testing...</title>
-        <style>
-            table, th, td {
-                border: 1px solid black;
-                border-collapse: :collapse;
-            }
-            th, td {
-                padding: 10px;
-            }
-        </style>
-        <script src="/socket.io/socket.io.js"></script>
-        <script src="http://code.jquery.com/jquery-1.11.1.js"></script>
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mqtt = require('mqtt');
 
-        <script type="text/javascript">
+var manufacturer;
+var loc;
+var tagid;
+var socketConnections = [];
+var clients=[];
+var items = [];
+var all =[];
+var data;
+var topic;
+var on_mqtt;
+var mqtt_manu;
+var message;
 
-            //var socket = io.connect('http://54.200.3.119:3000');
-            var socket = io.connect('http://rfidproject.hjcelindro.co.uk:3000');
+var DBmanufacturer;
+var tagid;
 
-            socket.on('connection',function(){
+var client_res;
 
-                socket.on('subscribe',function(msg){
-                });
-            });
+var mysql = require('mysql');
+/*var connection = mysql.createConnection({
+    host: 'br-cdbr-azure-south-a.cloudapp.net', //using microsoft azure mySQL
+    user: 'bbbfe6f5303166',
+    password: '6a070d81',
+    database: 'RFIDtags'
+});*/
+var connection = mysql.createConnection({
+    host: 'iotproject.c0hpcx3lq7af.us-west-2.rds.amazonaws.com', //using microsoft azure mySQL
+    user: 'hjcelindro',
+    password: 'Hannah0914',
+    database: 'rfidtags'
+});
 
-            socket.on('mqtt',function(msg){
-                var id = msg.payload.id;
-                $('table').append('<tr id="'+id+'" ><td>'+id+'</td><br><td>'+String(msg.payload.location)+'</td><br><td>'+String(msg.payload.manufacturer)+'</td><br><td>'+String(msg.payload.message)+'</td><br><td align="center"><form><input type=button id="collect" value="Collect" style="width:40%" onclick="getItem()"></form></td></tr>');
-                socket.emit('time taken',new Date().getTime());
-            });
-            
-            socket.on('data_change',function(msg){
-                $('table tr td').remove();
-            });
-            
-            socket.on('update_clients',function(name){
-                //updateClients(name);
-                //window.alert("Welcome "+name+'!');
-            });
-            
-            socket.on('users connected',function(data){
-                $('#usersConnected').html('Users connected: ' + data)
-            });
-            
-            socket.on('client response',function(data){
-            });socket.emit('client response',rowID);
+//-----------------DATABASE CONNECTIONS-------
 
-            
-            function subscribeToTopic(){
-                var textbox = document.getElementById("textbox");
-                var output = document.getElementById("output");
-                var name = textbox.value;
-                var manufacturer = name.substr(0,1).toUpperCase() + name.substr(1);
-                output.value = "You are Subscribed, "+manufacturer;
-                
-                var subscribe = 'manufacturer/'+manufacturer;
-                socket.emit('subscribe',{topic: subscribe});
-                socket.emit('register',manufacturer);
-                
-                $('div').show('clients');
-                $('div').show('table');
-            }
-            
-            function sendResponse(){
-                console.log("emitting get item");
-                socket.emit('client response',rowID);
-            }
-            
-            function getItem(){
-                $('input[type=button]').click(function(){
-                    rowID = $(this).closest('tr').attr('id');
-                    confirm("Press a button!");
-                    sendResponse();
-                    socket.removeListener();
-                });
-            }
-            
-            
+//connect to the database
+connection.connect(function(err){
+    if(err) { //if connection fails
+        console.error("error connecting: "+err.stack); //display error code and message
+        return;
+    }
+    console.log("Connected as: "+connection.threadId); //confirmation of connection with threadID
+});
+
+//----------------------------------
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/All', function(req, res){
+    res.sendFile(__dirname + '/indexAll.html');
+});
+
+http.listen(3000, function(){
+    console.log('listening to port 3000');
+});
+
+//var client = mqtt.connect('mqtt://54.200.3.119:1883');
+//var client = mqtt.connect('mqtt://rfidproject.hjcelindro.co.uk:1883');
+var client = mqtt.connect('mqtt://rfidproject.hjcelindro.co.uk:1883');
+var server = mqtt.connect('mqtt://rfidproject.hjcelindro.co.uk:1883');
+
+//--------------------------------------------------
+
+io.sockets.on('connection',function(socket){
+    server.subscribe('response/manufacturer');
+    console.log("subscribed to server'");
     
-        </script>
-    </head>
-    <body>
-        <form action=" ">
-            <fieldset>
-                <label>Type your Company name: </label>
-                <input name="topic" id="textbox" type= "text" align="right" size="30"/></br>
-                <input type="button" value="Subscribe" onclick = "subscribeToTopic()"/>
-                <input type="reset" value="Logout" onclick="history.go(0)"/>
-                <input type="text" id="output"  size = "50"/>
-            </fieldset>
-        </form>
-        <div>
-            <div id = 'clients' style="display:none"><h2>Welcome Client!</h2></div>
-            <div id="usersConnected"></div>
-            <ul id = 'messages'><h3>Messages</h3></ul>
-        </div>
-        <div id='table'>
-            <table id = "t1" style='width:60%' >
-                <tr>
-                    <th>Tag ID</th>
-                    <th>Location</th>
-                    <th>Manufacturer</th>
-                    <th>Other Information</th>
-                    <th>Action</th>
-                </tr>
-            </table>
-        </div>
-    </body>
-</html>
+    
+    console.log('------------------------------');
+
+    
+    socketConnections++;
+    io.sockets.emit('users connected',socketConnections);
+    
+    var addedClient = false;
+    
+    socket.on('subscribe',function(data){
+        console.log('a user connected'+socket.id);
+        if(data.topic=='manufacturer/All'){
+            client.subscribe('manufacturer/');
+            console.log('------------------------------');
+            console.log('Subscribing to: manufacturer/');
+            manfacturer="All";
+        }
+        else{
+            client.subscribe(data.topic);
+            console.log('Subscribing to :'+data.topic);
+            topic =data.topic;
+            var split = topic.split('/');
+            manufacturer = split[1];
+        }
+        searchManufacturerDatabase();
+        //message="";
+    });
+    
+    socket.on('mqtt',function(data){});
+    socket.on('data_change',function(data){});
+    
+    socket.on('register',function(name){
+        socket.emit('update_clients',name);
+        socket.join(name); //join room for the manufacturer
+    });
+    socket.on('disconnect', function(){
+        socketConnections--;
+        io.sockets.emit('users connected',socketConnections);
+        console.log('user disconnected');
+    });
+    socket.on('time taken',function(data){
+        var mqtt_time = (data - on_mqtt)/1000;
+        //console.log("Time from mqtt to client: "+mqtt_time);
+    });
+    
+    socket.on('client response',function(data){
+        client_res=data;
+        var response = client_res+" "+socket.id;
+        client.publish('response/manufacturer',response);
+    });
+});
+ 
+client.on('message',function(topic,message){
+    on_mqtt = new Date().getTime();
+    console.log("Client.on "+String(message)+ " "+String(topic));
+    var split = topic.split('/');
+    if(topic=='manufacturer/'){
+        mqtt_manu="All";
+    }
+    else{
+        mqtt_manu = split[1];
+    }
+    if(mqtt_manu==manufacturer||mqtt_manu=='All'){
+        io.emit('data_change',{'topic':String(topic), 'payload':data});
+        searchManufacturerDatabase();
+    }
+});
+
+server.on('message',function(topic,message){
+    console.log("client responded with: "+ message);
+});
+
+//search Database for manufacturer
+function searchManufacturerDatabase(){
+    console.log("Join Table");
+    var pre_query = new Date().getTime();
+    //-----this is a query function that gets rfid data from the online database and compares with reader values                
+    connection.query('SELECT rfid.item_rfid,rfid.item_manufacturer,rfid.item_location, Sensor.location, Sensor.idSensor, readings.idSensor, readings.time, readings.dataReading FROM rfid INNER JOIN Sensor ON rfid.item_location=Sensor.location inner join readings on Sensor.location=readings.idSensor',function(err,rows){
+        if(err)throw err;
+        else{
+            var post_query = new Date().getTime();
+            var duration = (post_query-pre_query)/1000;
+            console.log('------------------------------');
+            console.log("database connection taken: "+duration);
+            console.log('Data receieved from database'); //display message that data has been acquired from the database
+            console.log('------------------------------');
+            
+            for(var i=0; i<rows.length;i++){
+                DBmanufacturer = rows[i].item_manufacturer;
+                tagid = rows[i].item_rfid; //to make coding easier
+                loc = rows[i].item_location;
+                var sensorData = rows[i].dataReading;
+                var time = rows[i].time;
+                var response_message=DBmanufacturer+" will collect item";                
+                io.to('All').emit('mqtt',{'topic':'manufacturer/All', 'payload':{id:tagid,location:loc,manufacturer:DBmanufacturer,message:sensorData},response:response_message});
+                
+                if(DBmanufacturer===manufacturer){
+                    data = {id:tagid,location:loc,manufacturer:DBmanufacturer,message:sensorData};
+                    items.push(data);
+                    io.to(manufacturer).emit('mqtt',{'topic':String(topic), 'payload':data});  
+                }
+            }
+        }//END ELSE STATEMENT
+    }); //END QUERY
+} //END searchManufacturerDatabase();
