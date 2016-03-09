341f127a220e9a527f9da006b125df5540d8c85d
hjcelindro <hjcelindro@yahoo.co.uk>
2016-03-09 14:48:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ io.sockets.on('connection',function(socket){
     
     socket.on('client response',function(data){
         client_res=data;
-        var response = client_res+" "+socket.id;
+        var response = client_res+" "+socket.socket.sessionid;
         client.publish('response/manufacturer',response);
     });
 });
