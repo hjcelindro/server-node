697f26108ee6600abc87730c166ccd35c9dbfada
hjcelindro <hjcelindro@yahoo.co.uk>
2016-05-22 16:24:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -198,7 +198,8 @@ function searchManufacturerDatabase(){
 
 //--------------Search from RFID MQTT Messge from Edison-------------------------
 function searchDatabase(id){
-    var string_id = JSON.stringify(id).substr(1,10); //RFID data from arduino is an object, so to extract data, convert data to string
+    var string_id = JSON.stringify(id);
+    //.substr(1,10); //RFID data from arduino is an object, so to extract data, convert data to string
     console.log("SRFID: "+string_id);
     //-----this is a query function that gets rfid data from the online database and compares with reader values
     var pre_query = new Date().getTime();
